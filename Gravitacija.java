80740925b90a78b59a167ffa47ee62895cc17ade
Matslo1234 <matevz.crcek@gmail.com>
2021-02-24 16:39:53 +0100

    Dodajanje izpisa

---
+++ Gravitacija.java
@@ -0,0 +1,11 @@
+/* Gravitacija  */
+
+public class Gravitacija {
+    public static void main(String[] args){
+        System.out.println("OIS je zakon!");
+    }
+
+    public void izpisi(double nadmorskaVisina, double gravitacijskiPospesek){
+        System.out.println("Nadmorska visina: "+nadmorskaVisina+" Gravitacijski pospesek: "+gravitacijskiPospesek);
+    }
+}
